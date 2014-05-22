48798e9d287942b8e69bfd000e826e5ee45e1365
TheZeken <jeremie-b@hotmail.fr>
2014-05-22 16:24:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
-using namespace std;
+#include <map>
 
+#include "fonction.cpp"
+
+using namespace std;
+ 
 int main()
 {
-  
+//char a;
+
+void (*pointeurSurFonction)(void);
+pointeurSurFonction = test;
+
+map<char, void*> lol;
+
+
+lol['a'] = &pointeurSurFonction;
+
+char j;
+
+(*pointeurSurFonction)();
+
+return 0;
+}
+
+
+//fct.cpp
+
+#include <iostream>
+
+using namespace std;
+ 
+void test ()
+{
+cout << "WALLAH ça marche bien";
 }
