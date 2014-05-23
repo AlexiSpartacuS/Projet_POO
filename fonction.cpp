32245641348a1d54bbc10a994de7b5ed8c6c4f85
AlexiSpartacuS <alexis3lom@gmail.com>
2014-05-23 10:15:58 +0200

    Create fonction.cpp

---
+++ fonction.cpp
@@ -0,0 +1,15 @@
+//fonction.cpp
+
+#include <iostream>
+
+using namespace std;
+ 
+void test ()
+{
+cout << "WALLAH ça marche bien"<<endl;
+}
+
+void test1 ()
+{
+cout << "BLBLBLBLBLBLBLBLBLB"<<endl;
+}
